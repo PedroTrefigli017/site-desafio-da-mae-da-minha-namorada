361c0e168e6f04426c4355eb9ad149ac57e3932d
PedroTrefigli017 <60278612+PedroTrefigli017@users.noreply.github.com>
2020-02-08 21:12:27 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const $logo = document.querySelectorAll('.logo')[0];
+const $navBar = document.querySelectorAll('.nav-bar')[0];
+window.addEventListener('scroll',toggleNavBar,false);
+function toggleNavBar() {
+    if (window.pageYOffset >= $logo.offsetHeight && $navBar.classList.contains('abs-pos')) {
+        $navBar.classList.remove('abs-pos');
+        $navBar.classList.add('fix-pos');
+    } else if (window.pageYOffset < $logo.offsetHeight && $navBar.classList.contains('fix-pos')) {
+        $navBar.classList.add('abs-pos');
+        $navBar.classList.remove('fix-pos');
+    }
+}
+
+// https://www.daniortiz.net
+    
